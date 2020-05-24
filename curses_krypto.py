0d9682ccc973c46bdeff52196b63c29d67ed59ce
Toke Friis <tokefriis@gmail.com>
2020-05-24 13:48:28 +0200

    ændret hastighed

--- curses_krypto.py
+++ curses_krypto.py
@@ -3,6 +3,13 @@ import curses
 import time
 
 
+def choose_crypt(stdscr, current_index, y_axis):
+    stdscr.attron(curses.color_pair(3))
+    stdscr.addstr(y_axis, 0, current_index)
+    stdscr.refresh()
+    stdscr.attroff(curses.color_pair(3))
+
+
 def slow(text, y_axis, stdscr):
     x_axis = 0
     for char in text:
@@ -17,14 +24,19 @@ def main(stdscr):
     y_axis = 0
     machine_color = 1
     user_input_color = 2
-    current_row = 0
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    # for normal teks som man ikke skrive
     curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    # for noget man selv skriver
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    # for selected
+    curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
+    # for det man skal bruge
     stdscr.attron(curses.color_pair(machine_color))
     # her starter den med at skrive
     slow('Giv din key', y_axis, stdscr)
     stdscr.attron(curses.color_pair(user_input_color))
-    stdscr.getstr(1, 0)
+    user_key = stdscr.getstr(1, 0)
     stdscr.clear()
     while True:
         stdscr.attron(curses.color_pair(machine_color))
@@ -32,6 +44,23 @@ def main(stdscr):
         y_axis += 1
         slow('2: Decrypt', y_axis, stdscr)
         y_axis += 1
+        menu = True
+        while menu:
+            key = stdscr.getch()
+            if key == curses.KEY_UP:
+                current_index = "1: Incrypt"
+                stdscr.attron(curses.color_pair(machine_color))
+                stdscr.addstr(y_axis - 1, 0, "2: Decrypt")
+                stdscr.refresh()
+                choose_crypt(stdscr, current_index, y_axis - 2)
+            if key == curses.KEY_DOWN:
+                current_index = "2: Decrypt"
+                stdscr.attron(curses.color_pair(machine_color))
+                stdscr.addstr(y_axis - 2, 0, "1: Incrypt")
+                choose_crypt(stdscr, current_index, y_axis - 1)
+            if key == curses.KEY_ENTER:
+                stdscr.addstr(10, 10, "pressed enter")
+                menu = False
         stdscr.attron(curses.color_pair(user_input_color))
         stdscr.get_wch(y_axis, 0)
         stdscr.attron(curses.color_pair(machine_color))
@@ -41,7 +70,7 @@ def main(stdscr):
         stdscr.getstr(y_axis, len('skriv besked: '))
         y_axis += 1
         # her printer den den krypterede eller dekrypterede beskede
-        stdscr.attron(curses.color_pair())
+        stdscr.attron(curses.color_pair(4))
         slow('(her skal koden stå)', y_axis, stdscr)
         y_axis += 1
 
--- kryptering.py
+++ kryptering.py
@@ -11,7 +11,7 @@ def slow(text):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(0.03)
+        time.sleep(0.1)
 
 
 def encoder(cleartext, key): # key gives som et hexadecimal-tal
